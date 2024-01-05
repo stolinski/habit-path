a8fe15df911124675229af680a23256bda3067ef
Scott Tolinski <scott.tolinski@gmail.com>
2024-01-05 16:05:21 -0700

    adds logging to server hools

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -11,4 +11,23 @@ neonConfig.fetchConnectionCache = true;
 const sql = neon(DATABASE_URL);
 export const db = drizzle(sql, { schema });
 
+export const auth: Handle = async ({ event, resolve }) => {
+	const cookie_string = event.request.headers.get('cookie') ?? '';
+	const cookies = parse_cookies(cookie_string);
+	console.log('cookies', cookies);
+	const response = await resolve(event);
+	return response;
+};
+
 export const handle: Handle = sequence(form_data);
+
+export const parse_cookies = (cookie_string: string) => {
+	if (cookie_string)
+		return cookie_string
+			.split(';')
+			.map((v) => v.split('='))
+			.reduce((acc: any, v) => {
+				acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+				return acc;
+			}, {});
+};
